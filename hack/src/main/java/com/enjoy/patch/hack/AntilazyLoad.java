0cac4c4dd986a0989b9f2e52fc6f487b3efb3ad9
stevenhao <haoyunlonghaha@gmail.com>
2024-01-14 21:58:52 +0800

    发布代码到本地
    Signed-off-by: stevenhao <haoyunlonghaha@gmail.com>

--- hack/src/main/java/com/enjoy/patch/hack/AntilazyLoad.java
+++
@@ -1,6 +0,0 @@
-package com.enjoy.patch.hack;
-
-public class AntilazyLoad {
-
-
-}
--- hack/src/main/java/com/steven/stevenPlugin/PatchPlugin.java
+++ hack/src/main/java/com/steven/stevenPlugin/PatchPlugin.java
@@ -12,20 +12,16 @@ import org.gradle.api.Project;
 public class PatchPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-//        project.getPlugins().hasPlugin(AppPlugin.class);
-        System.out.println("~~~~~~patchPlugin");
-
-
+        System.out.println("~~~~~ 11Plugin");
         project.getExtensions().create("steven", PatchExt.class);
-//        project.getExtensions().create("patch", PatchExt.class);
 
         project.afterEvaluate(new Action<Project>() {
            @Override
            public void execute(Project project) {
                PatchExt ext = project.getExtensions().findByType(PatchExt.class);
-               System.out.println("~~~~~" + ext.isDebugon());
-               System.out.println("~~~~~" + ext.getApplicationName());
-               System.out.println("~~~~~" + ext.getOutput());
+               System.out.println("~~~~~11zuimengde" + ext.isDebugon());
+               System.out.println("~~~~~11zuimengde" + ext.getApplicationName());
+               System.out.println("~~~~~11zuimengde" + ext.getOutput());
            }
        });
     }
